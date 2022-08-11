c74dbc9b8afb228347a3da0bb6e7f4e59db1a509
m0nclous <m0nclous@vk.com>
2022-08-12 02:06:44 +0600

    Добавлен PostType <Afisha>

--- includes/Core.php
+++ includes/Core.php
@@ -27,6 +27,8 @@ class Core
     {
         $this->loader = new Loader();
 
+        $this->define_global_hooks();
+
         wp_enqueue_script(SLUG . '-vue', plugin_dir_url(__FILE__) . '../assets/js/vue.js', [], VERSION);
 
         if (is_admin()) {
@@ -52,6 +54,48 @@ class Core
     {
         $this->loader->add_action('wp_enqueue_scripts', [PublicCore::class, 'enqueue_styles']);
         $this->loader->add_action('wp_enqueue_scripts', [PublicCore::class, 'enqueue_scripts']);
+    }
+
+    /**
+     * Регистрация всех общих хуков
+     */
+    private function define_global_hooks(): void
+    {
+        $this->loader->add_action('init', [$this, 'register_post_type_afisha']);
+    }
+
+    public function register_post_type_afisha(): void
+    {
+        register_post_type('afisha', [
+            'label' => null,
+            'labels' => [
+                'name' => 'Афиша',
+                'singular_name' => 'Элемент',
+                'add_new' => 'Добавить',
+                'add_new_item' => 'Добавление',
+                'edit_item' => 'Редактирование',
+                'new_item' => 'Новое',
+                'view_item' => 'Смотреть',
+                'search_items' => 'Искать',
+                'not_found' => 'Не найдено',
+                'not_found_in_trash' => 'Не найдено в корзине',
+                'parent_item_colon' => '',
+                'menu_name' => 'Афиша',
+            ],
+            'description' => '',
+            'public' => true,
+            'show_in_menu' => null,
+            'show_in_rest' => null,
+            'rest_base' => null,
+            'menu_position' => null,
+            'menu_icon' => null,
+            'hierarchical' => false,
+            'supports' => ['title', 'editor'],
+            'taxonomies' => [],
+            'has_archive' => false,
+            'rewrite' => true,
+            'query_var' => true,
+        ]);
     }
 
     /**
